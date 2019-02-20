c01ff3491b91322143cc162d4aafc3fddea592da
ipokolew <pokolewIlja@gmail.com>
2019-02-20 22:15:37 +0300

    some ref

--- main(test).cpp
+++ main(test).cpp
@@ -3,14 +3,15 @@
 #include <string>
 
 #include "vld.h" //Visual Leak Detector
+#include <vector>
 
 #define MyDebug
 
 #ifdef MyDebug
-int OPushBack;	//O(n)=k
-int OSetRandPtr;//O(n)=n/2
+int OPushBack;	//O(n)=k+n/2=n (k - push back, n/2 - set pointer to random element)
+int OSetRandPtr;//O(n)=n/2=n
 int OSer;		//O(n)=n
-int ODes;		//O(n)=n+n/2 add n elements+ find random element to set random pointer
+int ODes;		//O(n)=n+n/2 = n^2 (n - add n elements, n/2 - set pointer to random element)
 #endif //MyDebug
 
 class ListNode
@@ -75,69 +76,29 @@ public:
 		SetRandElementPtr(tmp, index);
 #ifdef MyDebug
 		std::cout << "randidx_= " << Tail->Rand->index_ << ' ';
-		//std::cout << "iii= " << iii << "  ";
 		std::cout << "count_= " << Count;
 		std::cout << "\n";
 #endif //MyDebug
 	};
-	void SetRandElementPtr(ListNode *src, unsigned int index)
-	{
-
-		if (index <= Count / 2) //go to random index from from head
-		{
-			ListNode *tmp = Head;
-			while (tmp->index_ != index)
-			{
-#ifdef MyDebug
-				++OSetRandPtr;
-#endif //MyDebug
-
-				tmp = tmp->Next;
-			}
-			src->Rand = tmp;
-		}
-		else //go to random index from from tail
-		{
-
-			ListNode *tmp = Tail;
-			while (tmp->index_ != index)
-			{
-#ifdef MyDebug
-				++OSetRandPtr;
-#endif //MyDebug
-
-				tmp = tmp->Prev;
-			}
-			src->Rand = tmp;
-		}
-#ifdef MyDebug
-		std::cout << " OSetRandPtr " << OSetRandPtr << " ";
-		OSetRandPtr = 0;
-#endif //MyDebug
-
-	}
 	void Serialize(std::FILE* s) const
 	{
 #ifdef MyDebug
 		std::cout << "\n";
 		std::cout << " ser  ";
 #endif //MyDebug
-
+		unsigned int offset = 0u;
 		unsigned int length = 0u;
 		std::fwrite(&Count, sizeof(unsigned int), 1, s);
-
 		//std::fwrite(this, sizeof(*this), 1, s);
-
 		for (ListNode* node = Head; node != nullptr; node = node->Next)
 		{
 #ifdef MyDebug
 			++OSer;
 #endif //MyDebug
-
 			length = node->Data.size();
 			std::fwrite(&length, sizeof(unsigned int), 1, s);
-			std::fwrite(&node->Rand->index_, sizeof(unsigned int), 1, s);
 			std::fwrite(&node->Data[0], sizeof(char), length, s);
+			std::fwrite(&node->Rand->index_, sizeof(unsigned int), 1, s);
 			//std::cout << node->Data << " ";
 		}
 #ifdef MyDebug
@@ -191,8 +152,8 @@ public:
 #endif //MyDebug
 			std::fread(&length, sizeof(unsigned int), 1, s);
 			data.resize(length);
-			std::fread(&index, sizeof(unsigned int), 1, s);
 			std::fread(&data[0], sizeof(char), length, s);
+			std::fread(&index, sizeof(unsigned int), 1, s);
 			pushBack(data, index);
 		}
 #ifdef MyDebug
@@ -201,15 +162,13 @@ public:
 		ODes = 0;
 		std::cout << "\n";
 #endif //MyDebug
-
 	}
-	void Show()
+	void Show() const
 	{
 #ifdef MyDebug
 		std::cout << "\n";
 		std::cout << " show ";
 #endif //MyDebug
-
 		/*ListNode *temp = tail_;
 
 		while (temp != NULL)
@@ -218,22 +177,55 @@ public:
 			temp = temp->prev_;
 		}
 		std::cout << "\n";*/
-
 		ListNode *temp = Head;
 #ifdef MyDebug
 		std::cout << "Count= " << Count << " " << "Head= " << Head << " Tail= " << Tail << " ";
 #endif //MyDebug
-
 		while (temp != NULL)
 		{
 #ifdef MyDebug
 			std::cout << "\n temp= " << temp << " randidx= " << temp->Rand->index_;
 #endif //MyDebug
-
 			std::cout << " data= " << temp->Data << " ";
 			temp = temp->Next;
 		}
 		std::cout << "\n";
+	}
+private:
+	void SetRandElementPtr(ListNode *src, unsigned int index) const
+	{
+
+		if (index <= Count / 2) //go to random index from head
+		{
+			ListNode *tmp = Head;
+			while (tmp->index_ != index)
+			{
+#ifdef MyDebug
+				++OSetRandPtr;
+#endif //MyDebug
+
+				tmp = tmp->Next;
+			}
+			src->Rand = tmp;
+		}
+		else //go to random index from tail
+		{
+			ListNode *tmp = Tail;
+			while (tmp->index_ != index)
+			{
+#ifdef MyDebug
+				++OSetRandPtr;
+#endif //MyDebug
+
+				tmp = tmp->Prev;
+			}
+			src->Rand = tmp;
+		}
+#ifdef MyDebug
+		std::cout << " OSetRandPtr " << OSetRandPtr << " ";
+		OSetRandPtr = 0;
+#endif //MyDebug
+
 	}
 
 public:
@@ -250,20 +242,17 @@ int main(int argc, char** argv)
 	{
 		std::string data = std::to_string(i);
 		serLst.pushBack(data);
-
 	}
 
 	std::FILE *f = std::fopen("ListRand.txt", "wb");
 	serLst.Serialize(f);
 	std::fclose(f);
-
 	serLst.Show();
 
 	ListRand desLst;
 	f = std::fopen("ListRand.txt", "rb");
 	desLst.Deserialize(f);
 	std::fclose(f);
-
 	desLst.Show();
 
 	system("pause");
