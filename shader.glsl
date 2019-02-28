f72d9f7e99e2e8b54cb039528aca07d92474fcd8
ipokolew <ipokolew@saber3d.ru>
2019-02-28 21:18:06 +0300

    transform refact

--- shader.glsl
+++ shader.glsl
@@ -1,32 +1,29 @@
 #pragma once
 
 #ifdef VERTEX
-//layout (location = 1) in vec3 aColor;
-//uniform vec4 ourColor;
-//out vec3 ourColor;
+// layout (location = 1) in vec3 aColor;
+// uniform vec4 ourColor;
+// out vec3 ourColor;
 
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoord;
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec2 aTexCoord;
 
 out vec2 TexCoord;
 
-//uniform mat4 transform;
+// uniform mat4 transform;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-void main()
-{//ourColor = aColor;
-	gl_Position = projection * view * model * vec4(aPos, 1.0f);
-	TexCoord = vec2(aTexCoord.x, aTexCoord.y);
+void main() { // ourColor = aColor;
+  gl_Position = projection * view * model * vec4(aPos, 1.0f);
+  TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
-#endif //VERTEX
-
-
+#endif // VERTEX
 
 #ifdef FRAGMENT
-//in vec3 ourColor;
+// in vec3 ourColor;
 
 out vec4 FragColor;
 in vec2 TexCoord;
@@ -34,8 +31,8 @@ in vec2 TexCoord;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
-void main()
-{
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.05);//*vec4(ourColor, 1.0);
+void main() {
+  FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord),
+                  0.05); //*vec4(ourColor, 1.0);
 }
-#endif //FRAGMENT
+#endif // FRAGMENT
--- shader.h
+++ shader.h
@@ -4,69 +4,67 @@
 
 #include "context.h"
 
-class ShaderManager
-{
+class ShaderManager {
 public:
-	ShaderManager(const char* name) : fstream_(name), src_(nullptr), ID_(glCreateProgram())
-	{
-		//shader compile-time error check section
-		int  success;
-		char infoLog[512];
+  ShaderManager(const char *name)
+      : fstream_(name), src_(nullptr), ID_(glCreateProgram()) {
+    // shader compile-time error check section
+    int success;
+    char infoLog[512];
 
-		//read shader code to src_
-		unsigned int length = fstream_.getFileBytesize();
-		src_ = new char[length + 1]; //+1 for '\0'
-		fstream_.read(src_, length);
-		src_[length] = '\0';
+    // read shader code to src_
+    unsigned int length = fstream_.getFileBytesize();
+    src_ = new char[length + 1]; //+1 for '\0'
+    fstream_.read(src_, length);
+    src_[length] = '\0';
 
-		const int types[2] = { GL_VERTEX_SHADER, GL_FRAGMENT_SHADER };
-		const char* info[2][2] {
-		{ "#version 330 core\n#define VERTEX\n#line 0\n", src_ },
-		{ "#version 330 core\n#define FRAGMENT\n#line 0\n", src_ }};
+    const int types[2] = {GL_VERTEX_SHADER, GL_FRAGMENT_SHADER};
+    const char *info[2][2]{
+        {"#version 330 core\n#define VERTEX\n#line 0\n", src_},
+        {"#version 330 core\n#define FRAGMENT\n#line 0\n", src_} };
 
-		for (size_t i = 0; i < 2; ++i)
-		{
-			GLuint shaderObj = glCreateShader(types[i]);
-			glShaderSource(shaderObj, 2, info[i], NULL);
-			glCompileShader(shaderObj);
+    for (size_t i = 0; i < 2; ++i) {
+      GLuint shaderObj = glCreateShader(types[i]);
+      glShaderSource(shaderObj, 2, info[i], NULL);
+      glCompileShader(shaderObj);
 
-			//shader compile-time errors check
-			glGetShaderiv(shaderObj, GL_COMPILE_STATUS, &success);
-			if (!success)
-			{
-				glGetShaderInfoLog(shaderObj, 512, NULL, infoLog);
-				std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << info[i] << '\n' << infoLog << std::endl;
-			}
-			
-			glAttachShader(ID_, shaderObj);
-			glDeleteShader(shaderObj);
-		}
+      // shader compile-time errors check
+      glGetShaderiv(shaderObj, GL_COMPILE_STATUS, &success);
+      if (!success) {
+        glGetShaderInfoLog(shaderObj, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n"
+                  << info[i] << '\n'
+                  << infoLog << std::endl;
+      }
 
-		delete[] src_; //TODO move to ~?
+      glAttachShader(ID_, shaderObj);
+      glDeleteShader(shaderObj);
+    }
 
-		glLinkProgram(ID_);
+    delete[] src_; // TODO move to ~?
 
-		//linked shader program compile-time errors check
-		glGetProgramiv(ID_, GL_LINK_STATUS, &success);
-		if (!success)
-		{
-			glGetProgramInfoLog(ID_, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-		}
-	}
-	~ShaderManager() { glDeleteProgram(ID_); };
+    glLinkProgram(ID_);
 
-	void use() const { glUseProgram(ID_); }
+    // linked shader program compile-time errors check
+    glGetProgramiv(ID_, GL_LINK_STATUS, &success);
+    if (!success) {
+      glGetProgramInfoLog(ID_, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                << infoLog << std::endl;
+    }
+  }
+  ~ShaderManager() { glDeleteProgram(ID_); };
 
-	void setInt(const char* name, int value) const
-	{
-		glUniform1i(glGetUniformLocation(ID_, name), value);
-	}
-	const GLuint getShaderID() const { return ID_; };
-	
+  void use() const { glUseProgram(ID_); }
+
+  void setInt(const char *name, int value) const {
+    glUniform1i(glGetUniformLocation(ID_, name), value);
+  }
+  const GLuint getShaderID() const { return ID_; };
+
 private:
-	GLuint ID_;
-	FStream fstream_;
-	char* src_;
+  GLuint ID_;
+  FStream fstream_;
+  char *src_;
 };
-#endif //H_SHADER
+#endif // H_SHADER
